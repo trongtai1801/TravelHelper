87b0b40d96239ca3a502dec97378f50603d24f5a
tailt <trongtai.codecomplete@gmail.com>
2019-04-04 19:36:25 +0700

    design login activity, issue/#13

---
+++ app/src/main/java/dut/t2/travelhepler/service/model/Profile.kt
@@ -0,0 +1,42 @@
+package dut.t2.travel_helper.service.model
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+data class Profile(
+    @SerializedName("id")
+    @Expose
+    var id: String,
+
+    @SerializedName("fullName")
+    @Expose
+    var fullName: String,
+
+    @SerializedName("address")
+    @Expose
+    var address: String,
+
+    @SerializedName("gender")
+    @Expose
+    var gender: Boolean,
+
+    @SerializedName("birthday")
+    @Expose
+    var birthday: String,
+
+    @SerializedName("occupation")
+    @Expose
+    var occupation: String,
+
+    @SerializedName("fluentLanguage")
+    @Expose
+    var fluentLanguage: String,
+
+    @SerializedName("email")
+    @Expose
+    var email: String,
+
+    @SerializedName("phoneNumber")
+    @Expose
+    var phoneNumber: String
+)
---
+++ app/src/main/java/dut/t2/travelhepler/service/model/User.kt
@@ -0,0 +1,20 @@
+package dut.t2.travel_helper.service.model
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+data class User (
+    @SerializedName("UserName")
+    @Expose
+    var userName: String?,
+
+    @SerializedName("Password")
+    @Expose
+    var password: String?
+) {
+    data class builder(var userName: String? = null, var password: String? = null) {
+        fun userName(userName: String) = this.apply { this.userName = userName }
+        fun password(password: String) = this.apply { this.password = password }
+        fun build() = User(this.userName, this.password)
+    }
+}
