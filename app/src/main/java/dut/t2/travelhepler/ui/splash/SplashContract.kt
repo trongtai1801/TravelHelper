493aa021325888ddcd5c90216e2ef127678546bd
tailt <trongtai.codecomplete@gmail.com>
2019-04-04 19:13:14 +0700

    implement splash activity, issue/#8

---
+++ app/src/main/java/dut/t2/travelhepler/ui/splash/SplashContract.kt
@@ -0,0 +1,14 @@
+package dut.t2.travelhepler.ui.splash
+
+import dut.t2.basemvp.base.BaseView
+
+interface SplashContract {
+
+    interface SplashView : BaseView {
+
+    }
+
+    interface SplashPresenter {
+
+    }
+}
