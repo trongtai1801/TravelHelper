2ae4f3b80a01cf1e37520480ba1121d23938eb51
tailt <trongtai.codecomplete@gmail.com>
2019-04-05 16:30:18 +0700

    design main activity, issue/#23

--- app/src/main/java/dut/t2/travelhepler/ui/main/MainActivity.kt
+++ app/src/main/java/dut/t2/travelhepler/ui/main/MainActivity.kt
@@ -2,10 +2,7 @@ package dut.t2.travelhepler.ui.main
 
 import dut.t2.travelhelper.base.BaseActivity
 import dut.t2.travelhepler.R
-import dut.t2.travelhepler.utils.Constant
-import dut.t2.travelhepler.utils.SharedPrefs
 import kotlinx.android.synthetic.main.actionbar.*
-import kotlinx.android.synthetic.main.activity_main.*
 import org.androidannotations.annotations.EActivity
 
 @EActivity(R.layout.activity_main)
@@ -16,6 +13,5 @@ class MainActivity : BaseActivity<MainContract.MainView, MainPresenterImpl>(), M
     override fun afterViews() {
         tv_actionbar_title.setText(getString(R.string.dashboard))
         mActionBar!!.setDisplayHomeAsUpEnabled(false)
-        tv_main.setText(SharedPrefs.getInstance().get(Constant.ACCESS_TOKEN, String::class.java))
     }
 }
