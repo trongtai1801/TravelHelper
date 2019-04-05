7669e921557bc45c5c4f4c9841362bdb6d540e1d
tailt <trongtai.codecomplete@gmail.com>
2019-04-05 16:50:54 +0700

    init main fragments, issue/#23

---
+++ app/src/main/java/dut/t2/travelhepler/ui/main/dashboard/DashboardFragment.kt
@@ -0,0 +1,9 @@
+package dut.t2.travelhepler.ui.main.dashboard
+
+import android.support.v4.app.Fragment
+import dut.t2.travelhepler.R
+import org.androidannotations.annotations.EFragment
+
+@EFragment(R.layout.fragment_dashboard)
+class DashboardFragment :  Fragment(){
+}
---
+++ app/src/main/java/dut/t2/travelhepler/ui/main/more/MoreFragment.kt
@@ -0,0 +1,10 @@
+package dut.t2.travelhepler.ui.main.more
+
+import android.support.v4.app.Fragment
+import dut.t2.travelhepler.R
+import org.androidannotations.annotations.EFragment
+
+@EFragment(R.layout.fragment_more)
+class MoreFragment : Fragment() {
+
+}
